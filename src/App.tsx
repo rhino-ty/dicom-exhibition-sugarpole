328aba44bff8e8fc409c06dddc14e3fc230b4920
Rhino-ty <okko8522@gmail.com>
2024-04-02 16:27:14 +0900

    feat: App.tsx에 DICOMViewer를 map으로 설정해 6개가 다 나오도록 설정
    추후 2개씩 나오도록 설정 예정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import DICOMViewer from './components/dicom-viewer';
 
 function App() {
+  localStorage.setItem('debug', 'cornerstoneTools');
+  const dicomSourceArr: any = [
+    {
+      dicomFileName: 'dicom1.dcm',
+    },
+    {
+      dicomFileName: 'dicom2.dcm',
+    },
+    {
+      dicomFileName: 'dicom3.dcm',
+    },
+    {
+      dicomFileName: 'dicom4.dcm',
+    },
+    {
+      dicomFileName: 'dicom5.dcm',
+    },
+    {
+      dicomFileName: 'dicom6.dcm',
+    },
+  ];
   return (
     <main className='container mx-auto h-screen'>
       <h1>DICOM Viewer</h1>
-      <DICOMViewer dicomFileName='dicom1.dcm' />
+      {dicomSourceArr.map((item: any, idx: number) => (
+        <DICOMViewer key={idx} dicomFileName={item.dicomFileName} />
+      ))}
     </main>
   );
 }
