0a8ed3e6ee8ba4b3d79fc6fd9c1abf7774780084
Rhino-ty <okko8522@gmail.com>
2024-04-02 12:42:17 +0900

    chore: prettier 설치 및 prettier 설정 파일 추가
    eslint-config-prettier 라이브러리는 eslint와 충돌될 수도 있는 부분 고쳐줄 라이브러리

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  jsxSingleQuote: true,
+  singleQuote: true,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  bracketSpacing: true,
+
+  // npm install -D prettier prettier-plugin-tailwindcss
+  plugins: ['prettier-plugin-tailwindcss'],
+};
