8bf6d53b43c488766e5325f50d36fef373a844c3
Rhino-ty <okko8522@gmail.com>
2024-04-02 16:38:07 +0900

    refactor: cornerstoneTools.init() 에러가 고쳐지지 않아 일단 cornerstone-core으로만 기능 구현하도록 결정 및 cornerstone-tools 라이브러리 삭제

--- src/components/dicom-viewer.tsx
+++ src/components/dicom-viewer.tsx
@@ -8,7 +8,6 @@
 
 import React, { useEffect, useRef } from 'react';
 import * as cornerstone from 'cornerstone-core';
-import * as cornerstoneTools from 'cornerstone-tools';
 import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
 import * as dicomParser from 'dicom-parser';
 
@@ -29,8 +28,6 @@ const DICOMViewer: React.FC<DICOMViewerProps> = ({ dicomFileName }) => {
     cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
     cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
     cornerstone.enable(element);
-    // Init cornerstone tools
-    cornerstoneTools.init();
 
     // 파일 경로 설정 (예: 'dicomFileName' prop은 'yourfile.dcm'과 같은 값이어야 함)
     const imageId = `wadouri:${window.location.origin}/dicom/${dicomFileName}`;
@@ -38,21 +35,6 @@ const DICOMViewer: React.FC<DICOMViewerProps> = ({ dicomFileName }) => {
     cornerstone.loadImage(imageId).then((image) => {
       cornerstone.displayImage(element, image);
     });
-
-    // cornerstoneTools의 줌 기능
-    const ZoomTool = cornerstoneTools.ZoomTool;
-
-    cornerstoneTools.addTool(cornerstoneTools.ZoomTool, {
-      // Optional configuration
-      configuration: {
-        invert: false,
-        preventZoomOutsideImage: false,
-        minScale: 0.1,
-        maxScale: 20.0,
-      },
-    });
-
-    cornerstoneTools.setToolActive('Zoom', { mouseButtonMask: 1 });
 
     return () => {
       cornerstone.disable(element);
