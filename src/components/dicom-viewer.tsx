1350a9b8c3bdab20cfa60a1bdc550645e6419ad3
Rhino-ty <okko8522@gmail.com>
2024-04-02 16:28:23 +0900

    feat: DICOMViewer 컴포넌트에 cornerstoneTools 초기화 및 Zoom Tool 적용
    하지만 cornerstoneTools 초기화하는 과정인 cornerstoneTools.init()에서 에러가 나서 수정 중.
    Uncaught TypeError: Cannot read properties of undefined (reading 'EVENTS')

--- src/components/dicom-viewer.tsx
+++ src/components/dicom-viewer.tsx
@@ -8,6 +8,7 @@
 
 import React, { useEffect, useRef } from 'react';
 import * as cornerstone from 'cornerstone-core';
+import * as cornerstoneTools from 'cornerstone-tools';
 import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
 import * as dicomParser from 'dicom-parser';
 
@@ -28,36 +29,30 @@ const DICOMViewer: React.FC<DICOMViewerProps> = ({ dicomFileName }) => {
     cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
     cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
     cornerstone.enable(element);
+    // Init cornerstone tools
+    cornerstoneTools.init();
 
-    // 파일 경로 변경 (예: 'dicomFileName' prop은 'yourfile.dcm'과 같은 값이어야 함)
+    // 파일 경로 설정 (예: 'dicomFileName' prop은 'yourfile.dcm'과 같은 값이어야 함)
     const imageId = `wadouri:${window.location.origin}/dicom/${dicomFileName}`;
 
     cornerstone.loadImage(imageId).then((image) => {
       cornerstone.displayImage(element, image);
-
-      let zoom = 1; // 기본 Zoom 레벨 설정
-
-      // 마우스 휠 이벤트로 Zoom 조절
-      const onWheel = (e: WheelEvent) => {
-        e.preventDefault();
-        const viewport = cornerstone.getViewport(element);
-
-        // viewport가 undefined일 수도 있으므로 조건 설정
-        if (viewport) {
-          zoom += e.deltaY * -0.001; // Wheel 이벤트에 따라 zoom 값 조정
-          zoom = Math.min(Math.max(0.125, zoom), 4); // Zoom 값 제한
-
-          viewport.scale = zoom; // viewport에 zoom 적용
-          cornerstone.setViewport(element, viewport);
-        }
-      };
-
-      element.addEventListener('wheel', onWheel);
-
-      return () => {
-        element.removeEventListener('wheel', onWheel);
-      };
     });
+
+    // cornerstoneTools의 줌 기능
+    const ZoomTool = cornerstoneTools.ZoomTool;
+
+    cornerstoneTools.addTool(cornerstoneTools.ZoomTool, {
+      // Optional configuration
+      configuration: {
+        invert: false,
+        preventZoomOutsideImage: false,
+        minScale: 0.1,
+        maxScale: 20.0,
+      },
+    });
+
+    cornerstoneTools.setToolActive('Zoom', { mouseButtonMask: 1 });
 
     return () => {
       cornerstone.disable(element);
